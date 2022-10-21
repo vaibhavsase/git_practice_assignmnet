5a94375791fb8a808c5e8ddab0a85dc270ea9508
unknown <sasevaibhav00@gmail.com>
2022-10-21 18:55:26 +0530

    prime number is added

---
+++ Prime.js
@@ -0,0 +1,15 @@
+
+let n=7
+
+let count=0
+for(let i=1;i<=9;i++){
+  if(n%i===0){
+    count++
+  }
+}
+if(count===2){
+  console.log("Number is prime")
+}else{
+  console.log("Number is  not prime")
+  
+}
