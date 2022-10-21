06f471ae4854cd7f333c8eee4eaa8cebcac0d3ce
unknown <sasevaibhav00@gmail.com>
2022-10-21 18:58:40 +0530

    add prime number code

--- Prime.js
+++ Prime.js
@@ -1,15 +1,13 @@
+let n = 7;
 
-let n=7
-
-let count=0
-for(let i=1;i<=9;i++){
-  if(n%i===0){
-    count++
+let count = 0;
+for (let i = 1; i <= 9; i++) {
+  if (n % 1 === 0) {
+    count++;
   }
 }
-if(count===2){
-  console.log("Number is prime")
-}else{
-  console.log("Number is  not prime")
-  
+if (count === 2) {
+  console.log("Number is prime");
+} else {
+  console.log("Number is  not prime");
 }
